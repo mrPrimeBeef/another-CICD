7eaf3378b22713510b6c7421f6391d8ffde43667
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-03-19 15:00:03 +0100

    new push

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -4,7 +4,6 @@ import dat.config.ApplicationConfig;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
         ApplicationConfig.startServer(7070);
     }
 }
