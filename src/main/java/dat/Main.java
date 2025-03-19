ae7d4a8e80d11c5f20f68144330409eb2254a00f
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2025-03-19 14:35:31 +0100

    hello

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -3,8 +3,8 @@ package dat;
 import dat.config.ApplicationConfig;
 
 public class Main {
-
     public static void main(String[] args) {
+        System.out.println("hello");
         ApplicationConfig.startServer(7070);
     }
 }
